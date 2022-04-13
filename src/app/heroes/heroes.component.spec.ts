9ad8acbb00058d4cc649705ec2aab9ac9d213fc2
Robin Bette <robinbette@hotmail.com>
2022-04-13 10:48:42 +0200

    add comments

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -13,7 +13,11 @@ describe("HeroesComponent", () => {
       { id: 3, name: "Superguy", strength: 55 },
     ];
 
-    // create mock and tell jasmine which methods are needed
+    // create mock object that we can control.
+    // we can tell which methods it has and what does methods should return when called
+    // and we can ask it, what methods were called in a test
+    // if the object has no methods, we leave it blank
+    // so it has three methods in our case (from the heroService)
     mockHeroService = jasmine.createSpyObj([
       "getHeroes",
       "addHero",
@@ -25,18 +29,38 @@ describe("HeroesComponent", () => {
 
   describe("delete", () => {
     it("should remove the indicated hero from the heroes list", () => {
-      // tell delete method to return an observable (because of subscription)
-      // .and. is a mock object created by jasmine
+      // we need our mockobject to return an observable when deletHero is called:
+      //
+      // we are just subscribing to this method and do not care what DATA it contains inside
+      // .and. is a mock object created by jasmine - AND we can tell it to return the VALUE that we pass in the returnValue method
       // one of the easiest ways to create an observable is using rxjs' of
-      // observable that has one value, true, which satisfies the requirements of this test
+      // observable that has one value, true, which satisfies the requirements of this test,
+      // as we do not care about the DATA, as there is solely subscribed to
       mockHeroService.deleteHero.and.returnValue(of(true));
 
       // populate heroes property with sample data
+      // ngOnInit (getHoroes) is not running as we are performing an isolated test
+      // could also call onInit here
       component.heroes = HEROES;
 
       component.delete(HEROES[2]);
 
+      // state based test: test that state of component has changed
       expect(component.heroes.length).toBe(2);
     });
+
+    // interaction test: check if a certain interaction happened between tested class and some collaborated class (eg here; the hero service)
+    // this method is needed as we need to ensure the deleteHero method in the service was called
+    it("should call deleteHero with the correct hero", () => {
+      // quite some duplication here, but that is okay, as we are telling a story inside of these tests
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+    });
+
+    it("should subscribe to the deleteHero call", () => {});
   });
 });
