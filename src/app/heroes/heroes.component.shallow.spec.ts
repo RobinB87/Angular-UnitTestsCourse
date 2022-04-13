f8adb49d4402b7d247ba32286a67f6dc81a9a662
Robin Bette <robinbette@hotmail.com>
2022-04-13 15:17:56 +0200

    add deep integration test

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,11 +1,11 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { Component, Input } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 import { of } from "rxjs";
 import { Hero } from "../hero";
-import { By } from "@angular/platform-browser";
 
 describe("HeroesComponent (shallow tests)", () => {
   let fixture: ComponentFixture<HeroesComponent>;
