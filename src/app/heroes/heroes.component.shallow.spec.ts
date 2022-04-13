535fe49118deebb0436cd8bd4ec31da8d32255f4
Robin Bette <robinbette@hotmail.com>
2022-04-13 14:34:01 +0200

    mock child component

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 
 import { HeroService } from "../hero.service";
 import { HeroesComponent } from "./heroes.component";
 import { of } from "rxjs";
+import { Hero } from "../hero";
 
 describe("HeroesComponent (shallow tests)", () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
+
+  // just copy hero component parts
+  // remove styleUrls
+  // rename templateUrl to template and make it really easy, like:
+  // hero should not necessarily be typed as Hero, as it is a fake component
+  // remove export before class
+  // ensure to rename that it is obvious that is a fake class
+  // add the class to the TestBed declarations
+  @Component({
+    selector: "app-hero",
+    template: "<div></div>",
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+  }
 
   beforeEach(() => {
     HEROES = [
@@ -24,12 +41,13 @@ describe("HeroesComponent (shallow tests)", () => {
     ]);
 
     TestBed.configureTestingModule({
-      declarations: [HeroesComponent],
+      declarations: [HeroesComponent, FakeHeroComponent],
       // normally you would just inject HeroService with providers: [HeroService]
       // but now when somebody asks for the heroservice, we say use the mock
       providers: [{ provide: HeroService, useValue: mockHeroService }],
       // ignore child component for now
-      schemas: [NO_ERRORS_SCHEMA],
+      // this normally may have undesired side effects though (eg a typo in a template)
+      // schemas: [NO_ERRORS_SCHEMA],
     });
 
     fixture = TestBed.createComponent(HeroesComponent);
