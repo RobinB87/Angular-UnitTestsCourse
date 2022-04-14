443920c1126de23ac06938b2eee46bc0014a611a
Robin Bette <robinbette@hotmail.com>
2022-04-14 09:18:09 +0200

    use fakeAsync, tick and/or flush

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,11 +1,16 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+} from "@angular/core/testing";
 import { ActivatedRoute } from "@angular/router";
 import { Location } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { of } from "rxjs";
 
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroService } from "./../hero.service";
-import { of } from "rxjs";
 
 describe("HeroDetailComponent", () => {
   let fixture: ComponentFixture<HeroDetailComponent>;
@@ -59,18 +64,18 @@ describe("HeroDetailComponent", () => {
     );
   });
 
-  // done param to our callback (second argument of it function)
-  // now jasmine nows it is async
-  it("should call updateHero when save is called", (done) => {
+  // with fakeAsync and tick you avoid 'really' waiting 250 ms
+  // with flush you tell to look at the 'zone' for any tasks that are waiting
+  // if there are any, then fastforward the clock until those tasks are finished
+  it("should call updateHero when save is called", fakeAsync(() => {
     // empty object as code ignores return value
     mockHeroService.updateHero.and.returnValue(of({}));
     fixture.detectChanges();
 
     fixture.componentInstance.save();
+    // tick(250);
+    flush();
 
-    setTimeout(() => {
-      expect(mockHeroService.updateHero).toHaveBeenCalled();
-      done();
-    }, 300);
-  });
+    expect(mockHeroService.updateHero).toHaveBeenCalled();
+  }));
 });
